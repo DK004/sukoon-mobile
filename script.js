02c3973e4a098bdd2b463f610b05ecf8d6acb0d7
Arpit Alkari <71004336+DK004@users.noreply.github.com>
2023-02-18 18:27:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -132,59 +132,74 @@ function playPause(){
     
 
 // volume options
+var slider = document.getElementsByClassName("slider");
+
 function RainVol(value) {
   Rain.volume(value/100);
+  slider[0].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 function ThunderVol(value) {
   Thunder.volume(value/100);
+  slider[1].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 function WavesVol(value) {
   Wave.volume(value/100);
+  slider[2].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 function WindVol(value) {
   Wind.volume(value/100);
+  slider[3].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 function BirdsVol(value) {
   Birds.volume(value/100);
+  slider[4].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 function InsectsVol(value) {
   Insects.volume(value/100);
+  slider[5].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
-function RainVol(value) {
-  Rain.volume(value/100);
-}
 
 function FireVol(value) {
   Fire.volume(value/100);
+  slider[6].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 function TrafficVol(value) {
   Traffic.volume(value/100);
+  slider[7].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 function CafeVol(value) {
   Cafe.volume(value/100);
+  slider[8].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 
 
 function BowlVol(value) {
   Bowl.volume(value/100);
+  slider[9].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 function GamaVol(value) {
   Gama.volume(value/100);
+  slider[10].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 function OMVol(value) {
   OM.volume(value/100);
+  slider[11].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
 function WalkVol(value) {
   Walk.volume(value/100);
+    slider[12].style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
 }
+
+
+
 
 
 
@@ -193,32 +208,42 @@ function WalkVol(value) {
 
 
     // contact
-const nameOfSender = document.getElementById('feedback-body__email');
-const message = document.getElementById('feedback-body__message');
-const submit = document.getElementById('feedback-body__submit');
+// const nameOfSender = document.getElementById('feedback-body__email');
+// const message = document.getElementById('feedback-body__message');
+// const submit = document.getElementById('feedback-body__submit');
 
-submit.addEventListener('click', () => {
-    if(nameOfSender.value === null|| nameOfSender.value ==="" && message.value===""||message.value===null){
-        alert("Fill the requried* fields");
-        }else{
-            console.log("submitted")
-          setTimeout(() => {
-            sendMail();
-            nameOfSender.value="";
-            message.value="";
-          }, 2000);
-      }
-});
+// submit.addEventListener('click', () => {
+//     if(nameOfSender.value === null|| nameOfSender.value ==="" && message.value===""||message.value===null){
+//         alert("Fill the requried* fields");
+//         }else{
+  //             console.log("submitted")
+//           setTimeout(() => {
+//             sendMail();
+//             nameOfSender.value="";
+//             message.value="";
+//           }, 2000);
+//       }
+// });
 
 
-function sendMail(params){
-    var tempParams = {
-      from_name : nameOfSender.value,
-      to_name : "Sukoon",
-      message : message.value,
-      from_email : "Not requried", 
-    };
-    emailjs.send('service_fe9y6ov','template_u0lj0om',tempParams).then(function(res){
-      alert("Feedback sent successfully!");
-    });
-  }
+// function sendMail(params){
+  //     var tempParams = {
+//       from_name : nameOfSender.value,
+//       to_name : "Sukoon",
+//       message : message.value,
+//       from_email : "Not requried", 
+//     };
+//     emailjs.send('service_fe9y6ov','template_u0lj0om',tempParams).then(function(res){
+//       alert("Feedback sent successfully!");
+//     });
+//   }
+
+
+// for(let i=0; i < slider.length;i++){
+//   slider[i].onfocus = function() {
+//     var value = (this.value-this.min)/(this.max-this.min)*100;
+//     this.style.background = 'linear-gradient(to right, #388E3C 0%, #388E3C ' + value + '%, #C8E6C9 ' + value + '%, #C8E6C9 100%)';
+   
+//   }
+// }
+
